2d65cd20cf0b498e110dfb41fe0fa8f4f6910ea9
Daniel Mocanu <d.mocanu@student.fontys.nl>
2020-01-11 19:03:26 +0100

    show each igredient with picture from api

--- resources/views/ingredients/show.blade.php
+++ resources/views/ingredients/show.blade.php
@@ -5,56 +5,57 @@
     <div class="card shadow w-100 ml-5 mr-5">
         <div class="card-header">
             <div class="container mx-0 px-0">
-                <div class="row">
-                    <div class="col">
-                        IMAGE
-                    </div>
+                <div class="col">
+                    <h1 class="card-title" id="ingredientName">
+
+                     </h1>
                 </div>
                 <div class="row">
-                    <div class="col">
-                        <h1 class="card-title">
-                            INGREDIENT NAME
-                        </h1>
-
-                        <div class="d-flex flex-column justify-content-end">
-                            <div class="created_by">
-                                <p>
-                                    <i class="fas fa-user mr-2"></i>
-                                        USER
-                                </p>
-                            </div>
-                        </div>
+                    <div class="col" class="rounded float-left" id="picture" height="400" width="400">
                     </div>
                 </div>
             </div>
+        </div>
         </div>
         <div class="card-body">
             <div class="d-flex flex-row mb-3">
                 <i class="fas fa-list-ul fa-3x p-2"></i>
                 <div class="prep_time p-2 ml-2">
-                    <p>SCIENTIFIC NAME</br>
-                        SCIENTIFIC NAME
+                    <p id="scientificName">SCIENTIFIC NAME</br>
+
                 </div>
                 <div class="cooking_time p-2">
-                    <p>WIKIPEDIA LINK </br>
-                        WIKIPEDIA LINK
+                    <p id="wikilink">WIKIPEDIA LINK </br>
+
                 </div>
                 <div class="cooking_time p-2">
-                    <p>GROUP </br>
-                        GROUP
+                    <p id="group">GROUP </br>
+
                 </div>
             </div>
             <div class="card-body">
                 <div class="description">
-                <p>
-                    Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum  Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum  Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
-                    Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum  Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum  Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-                    Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum  Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum  Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
-                </p>
+                    <p id="ingredientDescription">
+                    </p>
                 </div>
             </div>
         </div>
     </div>
 </div>
 
+<script>
+var id = '{{request()->route("ingredient")}}';
+
+$.ajax({
+        url: "/api/ingredients/ " + id,
+        success: function(data) {
+            $('#ingredientName').append(data.name);
+            $('#ingredientDescription').append(data.description);
+            $('#group').append(data.group);
+            $('#wikilink').append('<a href="http://wikipedia.org/wiki/'+ data.wikipedia_id + '">' + data.name + '</a>');
+            $('#scientificName').append(data.name_scientific);
+            $('#picture').append('<img src="data:image/png;base64,' + data.picture + '" class="card-img-top">');
+        },
+    });
+</script>
 @stop
